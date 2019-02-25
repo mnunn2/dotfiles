055cf320e32d54b85ab643a5e91d0e40df354f6b
Mike Nunn <bonnie650@gmail.com>
2019-02-25 14:50:26 +0000

    initial commit

---
+++ linkme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for file in $HOME/dev/.[!.]*; do
+
+    link="$HOME/$(basename $file)"
+    echo "$link"
+
+    if [ -f "$link" ]; then
+
+        if ! [ -L "$link" ]; then
+            mv -f "$link" "/tmp/$(basename $link)" 
+            echo "i am not a link and got moved"
+        else
+            rm "$link"
+        fi
+    fi
+
+    ln -s "$file" "$link"
+
+done
